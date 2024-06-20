5ab1ab16a4119eae5ccaec66fa0315c75927b0d5
zedfaceless <zedfaces.fernandez@gmail.com>
2024-06-20 16:23:28 +0800

    New uploaded file

--- week02/bom.js
+++ week02/bom.js
@@ -1,10 +1,31 @@
-const input = document.querySelector('#favechap');
-const button = document.querySelectro('button');
-const list = document.querySelector('______');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button')
-deleteButton.textContent = '❌';
-li.textContent = input.value;
-li.append(deleteButton);
-list.append(li);
-input.value = '';
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+
+    // Create a delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+
+    li.textContent = input.value;
+
+    li.appendChild(deleteButton);
+
+    list.appendChild(li);
+
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
+  } else {
+    alert('Please enter a chapter title.');
+  }
+
+  input.focus();
+});
