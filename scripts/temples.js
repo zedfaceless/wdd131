2c1c017ef6fb4332f9132a71a5f8a7c8426a412a
zedfaceless <zedfaces.fernandez@gmail.com>
2024-06-20 18:18:34 +0800

    New uploaded file

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const mainnav = document.querySelector('nav')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
